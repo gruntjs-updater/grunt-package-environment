bea740f81968fed1a03373a7c9d95d61dd248ff6
Johnny Hall <johnny@recipher.co.uk>
2013-03-26 00:50:52 +0000

    Merge package contents rather than maintain entire files

--- tasks/package.js
+++ tasks/package.js
@@ -5,21 +5,26 @@
  * Copyright (c) 2013 Johnny Hall
  * Licensed under the MIT license.
  */
+var utile = require("utile");
 
 "use strict";
 
 var PACKAGE_FILE = "package.json"
-  , PACKAGE_FOLDER = "package";
+  , PACKAGE = "package";
 
 module.exports = function(grunt) {
   grunt.registerTask("package", "Swap package file based on environment", function(environment) {
 
-    if (environment == null) environment = PACKAGE_FOLDER;
+    var base = JSON.parse(grunt.file.read(PACKAGE + "/" + PACKAGE_FILE));
 
-    var environmentFile = environment + ".json"
-      , file = grunt.file.read(PACKAGE_FOLDER + "/" + environmentFile);
+    if (environment != null) {
+      var environmentFile = environment + ".json"
+        , environment = JSON.parse(grunt.file.read(PACKAGE + "/" + environmentFile));
 
-    grunt.file.write(PACKAGE_FILE, file);
-    grunt.log.ok("Package file swapped to " + environmentFile);
+      utile.mixin(base, environment);
+    }
+
+    grunt.file.write(PACKAGE_FILE, JSON.stringify(base, null, 2));
+    grunt.log.ok("Package file swapped to " + (environmentFile || PACKAGE_FILE));
   });
 };
